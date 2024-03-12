c6453396e66dd3599f4c3113cdb638e7c8d90c17
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2024-03-12 22:59:56 +0900

    Title: 자연수 n개의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240312/자연수 n개의 합/sum-of-n-natural-numbers.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+INF = int(2e9)
+
+left = 1
+right = INF
+max_num = 0
+
+while left <= right :
+    mid = (left + right) // 2
+
+    if mid * (mid + 1) //2 <= s :
+        left = mid + 1 # 좀더 큰 것 찾아보기
+        max_num = max(max_num, mid)
+
+    else : 
+        right = mid - 1
+
+print(max_num)
