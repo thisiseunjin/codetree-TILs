41a8106382b1db7fa15f68fa5ed2fe14670e8ac3
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2024-03-19 00:16:27 +0900

    Title: 정수 분배하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240319/정수 분배하기/distributing-integers.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int ,input().split())
+seq = []
+for _ in range(n) :
+    n = int(input())
+    seq.append(n)
+
+seq.sort()
+answer = 0
+left = 0
+right = 100001
+
+def ispossible(value) :
+    global m
+    count = 0
+    # 각 숫자를 // 2해서 더한 것이 m 이상이면 됨
+    for i in seq : 
+        count += i//value
+
+    if count >= m :
+        return True
+
+    else :
+        return False
+
+
+while(left <= right):
+    mid = (left + right) // 2
+
+    if ispossible(mid) :
+        # 좀 더 크게 해도 될듯
+        left = mid + 1
+        answer = max(answer, mid)
+    else : 
+        # 좀 더 작게해야한다 
+        right = mid - 1
+
+print(answer)
