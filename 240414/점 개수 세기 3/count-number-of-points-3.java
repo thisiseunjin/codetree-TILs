0a2f6967aeb953dda9783944b6aa3279c6896d2a
thisiseunjin <98890934+thisiseunjin@users.noreply.github.com>
2024-04-14 01:01:01 +0900

    Title: 점 개수 세기 3, Time: 3099ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 240414/점 개수 세기 3/count-number-of-points-3.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int q;
+    static int[] nums;
+    
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+
+        nums = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0;i<q;i++){
+            int result=0;
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            for(int j=0;j<n;j++){
+                if(start<=nums[j] && end>=nums[j]) result++;
+                if(nums[j]>end) break;
+            }
+
+            System.out.println(result);
+        }
+
+    }
+}
